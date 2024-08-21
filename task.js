f36c5221a1fcc5598c14b14a03cab7b4442613a5
raissa micheline <rimpuhwemanzi@gmail.com>
2024-08-21 18:17:10 +0200

    starting operating part

---
+++ task.js
@@ -0,0 +1,5 @@
+const daysInput = document.getElementById('days');
+const monthInput = document.getElementById('months');
+const yearInput = document.getElementById('years');
+const calculateBtn = document.getElementById('calc-btn');
+const resultSheet = document.getElementById('age-board');
